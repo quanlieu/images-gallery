5a87bdbba369527bbbde28370ab98f4eeb17596f
Quan Lieu <lieuchiquan@gmail.com>
2021-06-02 13:50:01 +0700

    Linting and code formatter

--- api/main.py
+++ api/main.py
@@ -11,29 +11,26 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("DEBUG", True))
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError('Please create .env.local file and insert the UNSPLASH_KEY')
+    raise EnvironmentError("Please create .env.local file and insert the UNSPLASH_KEY")
 
 app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = DEBUG
 
+
 @app.route("/new-image")
 def new_image():
-    print('request')
+    print("request")
     word = request.args.get("query")
-    headers = {
-        'Accept-Version': "v1",
-        'Authorization': f"Client-ID {UNSPLASH_KEY}"
-    }
-    params = {
-        'query': word
-    }
+    headers = {"Accept-Version": "v1", "Authorization": f"Client-ID {UNSPLASH_KEY}"}
+    params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
